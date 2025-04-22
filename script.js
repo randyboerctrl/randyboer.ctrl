c78e4a1f23d10a9a24f106964bebc0680edf3b89
Randy Boer <randyboer@mac.home>
2025-04-22 18:31:15 +0200

    Message

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // JavaScript to toggle between translation and original text
 function toggleTranslation(blockId) {
-    const textElement = document.querySelector(`#${blockId}-text`);
-    const translationElement = document.querySelector(`#${blockId}-text-translation`);
+    const textContainer = document.querySelector(`#${blockId}-text-container`);
+    const textElement = textContainer.querySelector(`#${blockId}-text`);
+    const translationElement = textContainer.querySelector(`#${blockId}-text-translation`);
     
     // Toggle visibility only for the text elements
     if (textElement && translationElement) {
