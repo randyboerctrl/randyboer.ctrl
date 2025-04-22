751d3e290d133dacae3f71c1157152f1c7e31e4d
Randy Boer <randyboer@mac.home>
2025-04-22 18:29:31 +0200

    Message

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ function toggleTranslation(blockId) {
     const textElement = document.querySelector(`#${blockId}-text`);
     const translationElement = document.querySelector(`#${blockId}-text-translation`);
     
-    // Toggle visibility
-    if (textElement.style.display === "none") {
-      textElement.style.display = "block";
-      translationElement.style.display = "none";
-    } else {
-      textElement.style.display = "none";
-      translationElement.style.display = "block";
+    // Toggle visibility only for the text elements
+    if (textElement && translationElement) {
+      if (textElement.style.display === "none") {
+        textElement.style.display = "block";
+        translationElement.style.display = "none";
+      } else {
+        textElement.style.display = "none";
+        translationElement.style.display = "block";
+      }
     }
   }
