db43862e714d209cb77a7e959527586b77582bea
Randy Boer <randyboer@mac.home>
2025-04-22 18:32:48 +0200

    Message

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// JavaScript to toggle between translation and original text
+// JavaScript to toggle between translation and original text for Bio only
 function toggleTranslation(blockId) {
     const textContainer = document.querySelector(`#${blockId}-text-container`);
     const textElement = textContainer.querySelector(`#${blockId}-text`);
     const translationElement = textContainer.querySelector(`#${blockId}-text-translation`);
     
-    // Toggle visibility only for the text elements
+    // Toggle visibility only for the Bio block text elements
     if (textElement && translationElement) {
       if (textElement.style.display === "none") {
         textElement.style.display = "block";
