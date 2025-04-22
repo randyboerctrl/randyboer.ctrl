8570a30e68ff73a22da92c5f20b1436f809d9b60
Randy Boer <randyboer@Randys-MacBook-Air.local>
2025-04-22 13:09:44 +0200

    Message

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('toggleLanguage').addEventListener('click', function() {
+    const bioEn = document.getElementById('bio-en');
+    const bioJp = document.getElementById('bio-jp');
+    
+    // Toggle the visibility of the two bio sections
+    if (bioEn.style.display === "none") {
+      bioEn.style.display = "block";
+      bioJp.style.display = "none";
+    } else {
+      bioEn.style.display = "none";
+      bioJp.style.display = "block";
+    }
+  });
