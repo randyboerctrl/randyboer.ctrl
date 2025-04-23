13a67e81e4df5527255bf28f6fcd6583be9daef1
Randy Boer <randyboer@mac.home>
2025-04-23 14:09:17 +0200

    Message

--- script.js
+++ script.js
@@ -22,3 +22,18 @@ window.addEventListener("DOMContentLoaded", () => {
     block.textContent = block.getAttribute("data-en");
   });
 });
+
+function toggleLanguage() {
+  const blocks = document.querySelectorAll(".block");
+
+  blocks.forEach(block => {
+    const en = block.getAttribute("data-en");
+    const ja = block.getAttribute("data-ja");
+    if (en && ja) {
+      block.innerHTML = isEnglish ? ja : en;
+    }
+  });
+
+  toggleBtn.textContent = isEnglish ? "EN" : "日本語";
+  isEnglish = !isEnglish;
+}
